9f5f3715be40ebcffe22ac722632e2646d98d8af
Manoah Tervoort <149895ja@gmail.com>
2024-02-14 14:22:59 +0100

    chore: personal page link

--- src/assets/js/habitats.js
+++ src/assets/js/habitats.js
@@ -22,10 +22,13 @@ const parseValue = (title, value) => {
 export const setHabitats = (habitats) => {
   const list = document.createElement("ul");
   habitats.forEach(({ title, value }) => {
-    list.appendChild(
-      document.createElement("li")
-    ).innerHTML = `${title}: ${parseValue(title, value)}`;
+    if (title !== "Personal Page") {
+      list.appendChild(
+        document.createElement("li")
+      ).innerHTML = `${title}: ${parseValue(title, value)}`;
+    }
   });
+
   const container = document.getElementById("habitat-container");
   container.innerHTML = "";
   container.appendChild(list);
